a13cbf82a41a633d12b5858d214521d4de4a144e
Tanvir(Software Engineer) <89591395+Tanvir-yzu@users.noreply.github.com>
2024-02-07 01:13:42 +0800

    Add files via upload

---
+++ DSA-Selection-Sort.py
@@ -0,0 +1,23 @@
+# Initialize an array of integers
+my_array = [2, 7, 3, 9, 12, 11, 5]
+
+# Calculate the length of array
+n = len(my_array)
+
+# Loop through each element in the array
+for i in range(n):
+    # Assume the current element is the smallest one found so far
+    min_i = i
+
+    # Loop through the remaining elements in the array
+    for j in range(i + 1, n):
+        # If a smaller element is found
+        if my_array[j] < my_array[min_i]:
+            # Update the index of the smallest element found so far
+            min_i = j
+
+    # Swap the current element with the smallest element found so far
+    my_array[i], my_array[min_i] = my_array[min_i], my_array[i]
+
+# Print the sorted array
+print(my_array)
